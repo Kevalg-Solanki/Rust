41c8f7c6d17716340f3d32ed0d0b1872e4c07f9b
Kevalg-Solanki <kevalg1950@gmail.com>
2026-07-05 20:02:37 +0530

    struct: complete employee increase salary by 10% using struct

--- emp_manage/src/main.rs
+++ emp_manage/src/main.rs
@@ -1,3 +1,28 @@
+struct Emp {
+    id: u32,
+    name: String,
+    salary: f32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    //CREATE MUTABLE EMPLOYEE
+    let mut emp1: Emp = Emp {
+        id: 10,
+        name: String::from("Jethalal"),
+        salary: 10000.00,
+    };
+
+    //PRINT EMPLOYEE
+    print_emp(&emp1);
+
+    //INCREASE SALARY BY 10%
+    emp1.salary = emp1.salary + emp1.salary * 0.10;
+
+    print_emp(&emp1);
+}
+
+fn print_emp(emp: &Emp) {
+    println!("Employee Id = {}", emp.id);
+    println!("Employee Name = {}", emp.name);
+    println!("Employee Salary = {}", emp.salary);
 }
