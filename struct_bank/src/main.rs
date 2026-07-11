0c5bc383d536a7751aaf75c4f9af0844c71517fa
Kevalg-Solanki <kevalg1950@gmail.com>
2026-07-11 22:00:41 +0530

    struct: complete bank details programme with impl method.

--- struct_bank/src/main.rs
+++ struct_bank/src/main.rs
@@ -4,22 +4,35 @@ struct BankAccount {
 }
 
 impl BankAccount {
-    fn deposit(&self, &amount: &String) {
+    fn deposit(&mut self, amount: f64) {
         self.balance += amount;
+    }
+
+    fn withdraw(&mut self, amount: f64) {
+        if self.balance > amount {
+            self.balance -= amount;
+        }
+        println!("Not Enough Balance");
+    }
+    fn display(&self) {
+        println!("Owner Name = {}", self.owner);
+        println!("Balance = {}", self.balance);
     }
 }
 
 fn main() {
     //Create Bank Account
-    let bank_account:BankAccount = BankAccount{
-        owner:String::from("Jeel"),
-        balance:13000.00
-    }
-    //
-}
+    let mut bank_account: BankAccount = BankAccount {
+        owner: String::from("Jeel"),
+        balance: 13000.00,
+    };
 
-fn print_bank_details(&bank_account:BankAccount)
-{
-    println!("Owner Name = {}",bank_account.owner);
-    println!("Balance = {}",bank_account.balance)
+    //Print details
+
+    bank_account.display();
+    //Deposit
+    bank_account.deposit(1000.00);
+    bank_account.display();
+    bank_account.withdraw(1000.00);
+    bank_account.display();
 }
